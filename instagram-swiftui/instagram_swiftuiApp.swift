8857557e0d54f979fa4a988668165d8f49307faa
Steve Hong <ponoponong@gmail.com>
2023-01-02 11:46:57 +0900

    Initial Commit

---
+++ instagram-swiftui/instagram_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_swiftuiApp.swift
+//  instagram-swiftui
+//
+//  Created by 홍성범 on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct instagram_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
